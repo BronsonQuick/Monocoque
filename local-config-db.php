c6b9b6cd61df09424c38b90a25c99ff0446e2187
Bronson Quick <bronson@bronsonquick.com.au>
2018-12-17 14:50:27 +1000

    Bring in the Chassis config files

---
+++ local-config-db.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is generated automatically by Puppet. Edit at your own risk.
+
+define( 'DB_NAME',     'wordpress' );
+define( 'DB_USER',     'wordpress' );
+define( 'DB_PASSWORD', 'vagrantpassword' );
+define( 'DB_HOST',     'localhost' );
+
+$table_prefix = 'wp_';
+
+defined( 'ABSPATH' ) or define( 'ABSPATH', '/vagrant/wp/' );
+defined( 'WP_CONTENT_DIR' ) or define( 'WP_CONTENT_DIR', '/vagrant/content' );
+
+
---
+++ local-config-extensions.php
@@ -0,0 +1,21 @@
+<?php
+
+// This is generated automatically by Puppet. Edit at your own risk.
+
+if ( file_exists( __DIR__ . '/extensions/_global/xdebug/local-config.php' ) )
+	require_once __DIR__ . '/extensions/_global/xdebug/local-config.php';
+
+if ( file_exists( __DIR__ . '/extensions/_global/sequelpro/local-config.php' ) )
+	require_once __DIR__ . '/extensions/_global/sequelpro/local-config.php';
+
+if ( file_exists( __DIR__ . '/extensions/_global/mailhog/local-config.php' ) )
+	require_once __DIR__ . '/extensions/_global/mailhog/local-config.php';
+
+
+if ( file_exists( __DIR__ . '/extensions/example/local-config.php' ) )
+	require_once __DIR__ . '/extensions/example/local-config.php';
+
+if ( file_exists( __DIR__ . '/extensions/tester/local-config.php' ) )
+	require_once __DIR__ . '/extensions/tester/local-config.php';
+
+
