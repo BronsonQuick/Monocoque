6863309bf8c9a9bf7b2d3c88a26f5067ebf7afdc
Bronson Quick <bronson@bronsonquick.com.au>
2018-12-20 12:47:33 +1000

    Setup WordPress database connection

--- local-config-db.php
+++ local-config-db.php
@@ -1,15 +1,13 @@
 <?php
 
-// This is generated automatically by Puppet. Edit at your own risk.
-
 define( 'DB_NAME',     'wordpress' );
 define( 'DB_USER',     'wordpress' );
-define( 'DB_PASSWORD', 'vagrantpassword' );
-define( 'DB_HOST',     'localhost' );
+define( 'DB_PASSWORD', 'wordpress' );
+define( 'DB_HOST',     'db' );
 
 $table_prefix = 'wp_';
 
-defined( 'ABSPATH' ) or define( 'ABSPATH', '/vagrant/wp/' );
-defined( 'WP_CONTENT_DIR' ) or define( 'WP_CONTENT_DIR', '/vagrant/content' );
+defined( 'ABSPATH' ) or define( 'ABSPATH', '/wp/' );
+defined( 'WP_CONTENT_DIR' ) or define( 'WP_CONTENT_DIR', '/content' );
 
 
