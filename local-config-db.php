2b127b9ac127ae61036f1706127bc9fc96bdf6da
Bronson Quick <bronson@bronsonquick.com.au>
2018-12-20 15:20:26 +1000

    Use environmental variables

--- local-config-db.php
+++ local-config-db.php
@@ -1,11 +1,11 @@
 <?php
 
-define( 'DB_NAME',     'wordpress' );
-define( 'DB_USER',     'wordpress' );
-define( 'DB_PASSWORD', 'wordpress' );
-define( 'DB_HOST',     'db' );
+define( 'DB_NAME',     getenv( 'DB_NAME' ) );
+define( 'DB_USER',     getenv( 'DB_USER' ) );
+define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ));
+define( 'DB_HOST',     getenv( 'DB_HOST' ) );
 
-$table_prefix = 'wp_';
+$table_prefix = getenv( 'DB_PREFIX' );
 
 defined( 'ABSPATH' ) or define( 'ABSPATH', '/wp/' );
 defined( 'WP_CONTENT_DIR' ) or define( 'WP_CONTENT_DIR', '/content' );
