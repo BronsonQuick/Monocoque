356e133bee6c0e42d105e6f21d53fac021cf3ea0
Bronson Quick <bronson@bronsonquick.com.au>
2018-12-20 15:06:45 +1000

    Remove the old reference to Puppet

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-// This is generated automatically by Puppet. Edit at your own risk.
-
 // WordPress view bootstrapper
 define( 'WP_USE_THEMES', true );
 require( 'wp/wp-blog-header.php' );
